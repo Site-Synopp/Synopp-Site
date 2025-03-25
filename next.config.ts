53b3b41b2f93769d764673fc2ec3cfc5ef93d2d3
github-second <agustingran@gmail.com>
2025-03-25 17:52:17 -0300

    fix: add next image config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [],
+    unoptimized: false,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+    formats: ["image/webp"],
+  },
 };
 
 export default nextConfig;
