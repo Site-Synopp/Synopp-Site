2f1b61de3873bf8ccb067e1587ff0fd2fb1cfb7c
josecalvano <josecalvano1546@gmail.com>
2025-03-27 14:47:49 -0300

    feat: New icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${InterFont.variable} bg-primary-700`}>
-      <head>
-        <link rel="stylesheet" />
+       <head>
+         <link rel="stylesheet" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <BrowserLanguageProvider>
         <body
